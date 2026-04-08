19e596a30bb6cf1cd7dadcc435fb7bb0d6afb103
Justin Walker <ggjjuu87@gmail.com>
2026-04-08 16:16:43 -0400

    Update Prototype.py

---
+++ Prototype.py
@@ -0,0 +1,4 @@
+#login information: username, password, email
+#Basic Information: Name, Date of Birth
+#Input information: Group Name, Event, Time of Event, Date of Event, Event Summary, 
+#Input information pt2: Event Location, Event Duration, Event Cost, Event Organizer, Event Contact Information
