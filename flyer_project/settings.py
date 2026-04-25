7fd27766db9a972123f76e2232c1c3dcfa4a81d9
CLiff <cliffnalldingvictor@gmail.com>
2026-04-24 22:01:59 -0400

    Fix login redirect and add login form template

--- flyer_project/settings.py
+++ flyer_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-okvki!ss3vk7yk_)ep6zfbfq-@s0qp5s5&hxb&^9igkbtycw%i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["willyouoylliw.pythonanywhere.com", 
                  "localhost",
